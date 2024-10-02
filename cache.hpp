73dc98d79b3b84ab15f3bcc209f7575a792a71f5
kxkxkxq <162305923+kxkxkxq@users.noreply.github.com>
2024-10-02 22:30:13 +0300

    redesigned caches::lirs::lirs()

--- cache.hpp
+++ cache.hpp
@@ -30,18 +30,39 @@ namespace caches
 
     template <typename KeyT, typename T> class lirs
     {
+        enum Csizes {hsize = 1, lsize = 99 }; //percentage of hirs and lirs lists sizes 
+        size_t csize = 0;
+        
+        struct list_t{size_t size_ = 0; std::list<KeyT> list_; };
+
         std::unordered_multimap<KeyT, pnode_t<KeyT, T>> hashtable; //we need to write : hashtable::insert({key, page_});
         std::deque<KeyT> tdeque;
-        std::list<KeyT> tlist;
+        list_t lhirs;      //part of cache for hirs elements (approximately 1% of cache size)
+        list_t llirs;      //part of cache for lirs elements (approximately 99% of cache size)
    
     public:
-      
-        unsigned csize = 0;
-
+    
         int process_request(const data::data_t<KeyT, T> &dref);
         void process_hashtable_pair(const std::pair< KeyT, pnode_t<KeyT, T>>& h); //i will need such func later
 
-        lirs(const unsigned sz) : csize(sz) {};
+        lirs(const unsigned sz) : csize(sz)
+        {   
+            const size_t hs = (sz * hsize / 100) + 1;
+            const size_t ls = sz - hs;
+
+            lhirs.size_ = hs;
+            llirs.size_ = ls;
+
+#if 1
+            std::cerr << "      cacehs::lirs::lirs() : hirs size == " << hs << "\n";
+            std::cerr << "      caches::lirs::lirs() : lirs size == " << ls << "\n";
+            std::cerr << "      caches::lirs.size == " << csize << "\n";
+            
+            std::cerr << "      llist.size_ == " << llirs.size_ << "\n";
+            std::cerr << "      lhirs.size_ == " << lhirs.size_ << "\n";
+#endif
+
+        };
     };
 
 }
@@ -67,8 +88,18 @@ int caches::lirs<KeyT, T>::process_request(const data::data_t<KeyT, T>& dref)
     std::cerr << "          pnode_t.itq == " << *itp->second.itq << "\n";         
 
     std::cerr << "      hashtable pair{KeyT, pnode_t<KeyT, T>} : ";
-    std::cerr << "{KeyT elem == " << itp->first << ", pnode_t<KeyT, T>.KeyT elem == " << itp->second.key_ << "}\n";
+    std::cerr << "{KeyT elem == " << itp->first << ", pnode_t<KeyT, T>.KeyT elem == " << itp->second.key_ << "}\n\n";
+
+    std::cerr << "      dqueue : ";
+    for(auto i = tdeque.begin(); i != tdeque.end(); ++i)
+        std::cerr << *i << " ";
+    std::cerr << "\n";
+    std::cerr << "      hashtable size == " << hashtable.size() << "\n";
 #endif
+
+
+
+
 
     return 0;
 }
