523fa3afdc7037909639030041a4ff0dc8bd0d2f
kxkxkxq <162305923+kxkxkxq@users.noreply.github.com>
2024-10-28 23:48:00 +0300

    Belady's cache algorithm

---
+++ belady.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+#include <queue>
+#include <vector>
+
+#include "data.hpp"
+#include "belady.hpp"
+
+int main()
+{
+    data::Data<int, std::string> idata;
+    idata.input_data();
+    caches::belady<int, std::string> bcache(idata.csize, idata.requests);
+
+    unsigned nhits = 0;
+    for(auto i = idata.requests.begin(), e = idata.requests.end(); i != e; ++i)
+        nhits += bcache.proccess_request(*i);
+
+    data::print_nhits(nhits);
+}
