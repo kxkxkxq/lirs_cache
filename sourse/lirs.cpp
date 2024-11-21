bb6d7a5709686b121299bc9dec3f8c1b838338ab
kxkxhh <sukhanov.is@phystech.edu>
2024-11-21 08:57:17 +0300

    some changes in ./sourse

--- sourse/lirs.cpp
+++ sourse/lirs.cpp
@@ -10,14 +10,17 @@ int main()
     long long size = 0, nrequests = 0;
     std::cin >> size >> nrequests;
     
-    if(std::cin.fail() || (size < 0) || (nrequests < 0))
+    if(std::cin.fail() || (size < 2) || (nrequests < 0))
     {
         std::cerr << "Input error\n";
+        if(size < 2)
+            std::cerr << "Minimum allowed size for LIRS cache : 2\n";
+        
         return 1;
     }
 
     caches::lirs<int> lcache{size};
-
+                            //  add check that csize != 1
     unsigned nhits = 0;
     for(unsigned i = 0; i < nrequests; ++i)
     {
